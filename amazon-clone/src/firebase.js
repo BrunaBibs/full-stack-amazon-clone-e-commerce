5c25cf176577d1dca32d655ed318562f8d8e23c6
Bruna Sousa <bruna_irina@hotmail.com>
2020-10-22 09:07:49 +0100

    added firebase config

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzk2MhXVtk7k-T4MXGfRF2b7yATcQMlzs",
+  authDomain: "clone-7d5bd.firebaseapp.com",
+  databaseURL: "https://clone-7d5bd.firebaseio.com",
+  projectId: "clone-7d5bd",
+  storageBucket: "clone-7d5bd.appspot.com",
+  messagingSenderId: "152908400039",
+  appId: "1:152908400039:web:b198e1c161390077b6e282",
+  measurementId: "G-L4S741QKLS"
+};
