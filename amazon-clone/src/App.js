d88b58e9ce51c5a579285902d3353e997382b4c0
Bruna Sousa <bruna_irina@hotmail.com>
2020-10-22 09:42:52 +0100

    working on header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
     <h1>Hello Clever Programmers, let's build Amazon Store🚀</h1>
+    <Header />
     </div>
   );
 }
