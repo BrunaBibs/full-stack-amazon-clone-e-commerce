0497f9e364394a204ad6a1d8c64bf8edf4243ff0
Bruna Sousa <bruna_irina@hotmail.com>
2020-10-22 10:04:59 +0100

    added material UI icons to the project, now styling the header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-    <h1>Hello Clever Programmers, let's build Amazon Store🚀</h1>
     <Header />
     </div>
   );
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
@@ -7,6 +8,7 @@ function Header() {
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
             <div className="header__search">
                 <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
             </div>
 
             <div className="header__nav">
