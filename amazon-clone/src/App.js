19262670e0280d4894959f2ca5112037aee3a017
Bruna Sousa <bruna_irina@hotmail.com>
2020-10-23 09:08:40 +0100

    adding the checkout functionality

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
 
     <Router>
       <div className="app">
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <h1>I AM THE CHECKOUT PAGE</h1>
-          </Route>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
+        <Header />
+          <Switch>
+            <Route path="/checkout">
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Home />
+             </Route>
+         </Switch>
       </div>
     </Router>
   );
